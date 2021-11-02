ce28ccd0625311778b2c36cb69eae02d51261374
Sumit <sumitkumar280601@gmail.com>
2021-11-02 10:04:56 +0530

    Tutorial 43 is completed

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const connectToMongo = require('./db')
+connectToMongo();
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Sumit Kumar!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
