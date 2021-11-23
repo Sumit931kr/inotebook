b218667056cc3a2b74462c33d31393b9be1f44c2
Sumit <sumitkumar280601@gmail.com>
2021-11-23 08:14:05 +0530

    Tutorial 52 is completed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,13 +1,83 @@
 const express = require('express')
 const router = express.Router()
+const Note = require('../modules/Note')
+const fetchUser = require('../Middleware/fetchUser');
+const { body, validationResult } = require('express-validator')
 
-router.get('/',(req, res)=>{
-    obj = {
-        a:"DOC",
-        number : 45
+// ROUTES 1 :- Get All the Notes Using : GET "/api/auth/fetchallnotes" . Login Required 
+
+router.get('/fetchallnotes', fetchUser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id })
+        res.json(notes)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send(" 16 Internal Server ERROR")
     }
-    res.json(obj)
 })
+
+// ROUTES 2 :- Add a New Notes using: POST "/api/auth/addnotes" . Login Required 
+
+router.post('/addnotes', fetchUser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
+        try {
+            const { title, description, tag } = req.body;
+
+            // If there are errors, return Bad request and the errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save()
+
+            res.json(savedNote)
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+    })
+
+
+
+// router.post('/addnotes', fetchUser, [
+//     body('title', 'Enter a Valid Title').isLength({ min: 3 }),
+//     body('description', 'Description Must be  5 Character ').isLength({ min: 5 }),
+
+// ], async (req, res) => {
+
+//     try {
+//         const { title, description, tag } = req.body;
+
+//         // If there are errros, return Bad request and the error
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({ errors: errors.array() });
+//         }
+
+//         const note = new Note({
+//             title, description, tag, user : req.user.id
+//         })
+
+//         const savedNote = await note.save()
+
+
+//         // const notes = await Notes.find({user : req.user.id})
+//         res.json(savedNote)
+
+//     } catch (error) {
+//         console.error(error.message);
+//         res.status(500).send(" 48 Internal Server ERROR")
+//     }
+// })
+
+
+
 
 
 module.exports = router
