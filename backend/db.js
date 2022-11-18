a5244b5de17a9f1f779a0fc6374e4a350b7cb122
Sumit kumar <90455637+Sumit931kr@users.noreply.github.com>
2022-11-18 13:38:51 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+const mongoURI = YOUR MONGO URI
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
